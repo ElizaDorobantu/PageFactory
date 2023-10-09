50aff44f45e34c231a85b42b91d12597c34e0329
Eliza Gutulov dorobantu <eliza.gutulov@yahoo.com>
2023-10-09 11:33:49 +0300

    curs35

---
+++ src/main/java/tests/FindBrokenLinks.java
@@ -0,0 +1,71 @@
+package tests;
+
+import java.net.MalformedURLException;
+import java.util.List;
+import java.util.ArrayList;
+import java.net.URL;
+
+import static org.testng.Assert.assertTrue;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import utils.BaseTest;
+
+public class FindBrokenLinks extends BaseTest{
+	
+	static List<String> brokenLinks = new ArrayList<String>();
+
+	@Test
+	public void testBrokenLinks() throws IOException {
+		
+		List<WebElement> links = driver.findElements(By.cssSelector("a[href*='http']"));
+		
+		System.out.println(links.size());
+		
+		System.out.println("---------------");
+		checkLinks("https://keyfood.ro/product-category/breads-bakery/");
+		System.out.println("---------------");
+		
+		System.out.println("-------Bad URL--------");
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		jse.executeScript("arguments[0].setAttribute('href','https://keyfood.ro/product-category/breads-bake/')",
+				links.get(2));
+		System.out.println("---------------");
+		
+		for (int i=0; i<10/*i<links.size()*/;i++) {
+			WebElement element = links.get(i);
+			String url = element.getAttribute("href");
+			
+			if(url==null) {
+				continue;
+			}
+			checkLinks(url);
+		}
+		assertTrue(brokenLinks.size()==0);
+	}
+	
+	public static void checkLinks(String linkUrl) throws IOException {
+		try {
+			URL url = new URL(linkUrl);
+			HttpURLConnection httpUrlConnection = (HttpURLConnection) url.openConnection();
+			httpUrlConnection.setConnectTimeout(2000);
+			httpUrlConnection.connect();
+			
+			if(httpUrlConnection.getResponseCode()==200) {
+				System.out.println(linkUrl + " "+httpUrlConnection.getResponseCode());
+			}
+			if(httpUrlConnection.getResponseCode()==404) {
+				System.out.println(linkUrl + " "+httpUrlConnection.getResponseCode());
+				brokenLinks.add(linkUrl);
+			}
+		}catch(MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/main/java/tests/LoginTest.java
+++ src/main/java/tests/LoginTest.java
@@ -11,8 +11,20 @@ import utils.ScreenShots;
 public class LoginTest extends BaseTest{
 
 	@Parameters({"user","pass"})
-	@Test
+	@Test(priority=1)
 	public void loginTest(String username,String password) {
+		app.click(app.menu.myAccountLink);
+		app.myAccount.loginInApp(username, password);
+		assertTrue(app.checkElementIsDisplayed(app.myAccount.usernameGreetings));
+		ScreenShots.screenshot(driver); 
+		assertTrue(app.myAccount.usernameGreetings.isDisplayed());
+		app.click(app.myAccount.logoutButton);
+		//cele 2 linii de mai sus cu assertTrue fac acelasi lucru
+	}
+	
+	@Parameters({"user2","pass"})
+	@Test(priority=2)
+	public void loginTestInvalid(String username,String password) {
 		app.click(app.menu.myAccountLink);
 		app.myAccount.loginInApp(username, password);
 		assertTrue(app.checkElementIsDisplayed(app.myAccount.usernameGreetings));
@@ -20,7 +32,6 @@ public class LoginTest extends BaseTest{
 		assertTrue(app.myAccount.usernameGreetings.isDisplayed()); 
 		//cele 2 linii de mai sus cu assertTrue fac acelasi lucru
 	}
-	
 //testul din PageObjectModel
 //public void loginInApp(String user, String pass) {
 //		
